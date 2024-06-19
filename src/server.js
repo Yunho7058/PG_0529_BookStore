3c834b54dc944349b7d30efb9da11179663f3b10
yunho <Yunho7058@github.com>
2024-06-19 23:40:18 +0900

    book

--- src/server.js
+++ src/server.js
@@ -1,41 +1,16 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import {memo} from 'react'
 
-interface dogImgInterface {
-  id: string;
-  dogUrl: string;
-}
+const Child = memo(funtion(){
+  console.log('실행')
+  return <div>자식</div>
+})
 
-export default function Dogs() {
-  const [dogImgArr, setDogImgArr] = useState<dogImgInterface[]>([]);
+function parents(){ 
 
-  useEffect(() => {
-    console.log("로드");
-
-    // key가 없으면 응답은 10개씩
-    const API_URL =
-      "https://api.thedogapi.com/v1/images/search?size=small&format=json&has_breeds=true&order=ASC&page=0&limit=10";
-    axios.get(API_URL).then((res) => {
-      console.log(res);
-      
-      // id값과 url만 저장
-      const gotData = res.data.map((dogImg: { id: string; url: string }) => ({
-        id: dogImg.id,
-        dogUrl: dogImg.url,
-      }));
-      setDogImgArr(gotData);
-    });
-  }, []);
+  let [count, setCount] = useState(0)
 
   return (
-    <div className="dog-imgs-container">
-      {dogImgArr &&
-        dogImgArr.map((dogImg: dogImgInterface) => (
-          <div key={dogImg.id} className="dog-img-card">
-            <img src={dogImg.dogUrl} />
-            <p>cute_{dogImg.id}</p>
-          </div>
-        ))}
-    </div>
-  );
+    <Child  />
+    <button onClick={()=>{ setCount(count+1) }}> + </button>
+  )
 }
